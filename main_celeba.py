42c3a88f71aa6ca6fe7b62e41b0d3014d73169a1
Saeed Salehi <saeed.salehi.n@gmail.com>
2024-10-14 14:40:43 +0200

    Update main_celeba.py

--- main_celeba.py
+++ main_celeba.py
@@ -23,24 +23,23 @@ random.seed(1984)
 parser = argparse.ArgumentParser()
 parser.add_argument('-n_epochs', type=int, default=32)
 parser.add_argument('-batch_size', type=int, default=128)
-parser.add_argument('-lr', type=float, default=0.0001)
-parser.add_argument('-l2', type=float, default=1e-4)
-parser.add_argument('-lm', type=float, default=0.0)
+parser.add_argument('-lr', type=float, default=1e-4)
+parser.add_argument('-l2', type=float, default=5e-4)
 parser.add_argument('-n_iter', type=int, default=1)
 parser.add_argument('-exase', type=str, default="default")
 parser.add_argument('-verbose', type=int, default=1)
 argus = parser.parse_args()
-data_path = r"../attention/data"
+data_path = r"./data"
 train_params = {
     "n_epochs": argus.n_epochs,
     "batch_size": argus.batch_size,
     "lr": argus.lr,
     "l2": argus.l2,
-    "lm": argus.lm,
-    "exase": argus.exase,
+    "exase": str(argus.exase),
     "dir": r"./results",
-    "milestones": [32, ],
-    "gamma": 0.2,
+    "milestones": [16, ],
+    "gamma": 0.1,
+    "max_grad_norm": 1.0,
 }
 
 model_params = {
@@ -49,19 +48,19 @@ model_params = {
     "out_dim": 2,  # output dimensions (could be larger than n_classes)
     "normalize": True,  # normalize input images
     "softness": 0.5,  # softness of the attention (scale)
-    "channels": (3, 4, 8, 16, 16, 32),  # channels in the encoder
+    "channels": (3, 4, 8, 8, 16, 16, 32, 32),  # channels in the encoder
     "residuals": False,  # use residuals in the encoder
     "kernels": 3,  # kernel size
     "strides": 1,  # stride
-    "paddings": "same",  # padding
+    "paddings": 1,  # padding
     "conv_bias": True,  # bias in the convolutions
     "conv_norms": None,  # normalization in the encoder
     "conv_dropouts": 0.0,  # dropout in the encoder
     "conv_funs": torch.nn.ReLU(),  # activation function in the encoder
     "deconv_funs": torch.nn.Tanh(),  # activation function in the decoder
     "deconv_norms": None,  # normalization in the decoder
-    "pools": (4, 2, 2, 2, 2),  # pooling in the encoder
-    "rnn_dims": (16, 8),  # dimensions of the RNN (First value is not RNN but FC)
+    "pools": 2,  # pooling in the encoder
+    "rnn_dims": (32, 8, ),  # dimensions of the RNN (First value is not RNN but FC)
     "rnn_bias": True,  # bias in the RNN
     "rnn_dropouts": 0.0,  # dropout in the RNN
     "rnn_funs": torch.nn.ReLU(),  # activation function in the RNN
@@ -70,8 +69,9 @@ model_params = {
     "task_bias": False,  # use tasks embeddings for the decoder channels  (additive)
     "task_funs": None,  # activation function for the tasks embeddings
     "rnn_to_fc": True,  # Whether to use the RNN layers or FC
-    "rnn_cat": False, # whether to concatenate the forward and backward RNN outputs
-    "use_bridges": False,  # whether to use a fancy bridge between the encoder and decoder
+    "trans_fun": torch.nn.Identity(),  # activation function for the bridge
+    'norm_mean': [0.5, 0.5, 0.5],  # mean for the normalization
+    'norm_std': [1.0, 1.0, 1.0],  # std for the normalization
 }
 
 # # tasks include the composer, key, params, datasets, dataloaders, loss weights, loss slices, and has prompt
@@ -83,16 +83,16 @@ tasks = OrderedDict({})
 tasks["Celeb"] = {
     "composer": CelebACrop,  # composer (torch Dataset)
     "key": 0,  # key for the task
-    "params": {"n_iter": argus.n_iter, "hair_dir": None, "in_dims": model_params["in_dims"], "padding": 0, "noise": 0.125, "which": 0},
+    "params": {"n_iter": argus.n_iter, "hair_dir": None, "in_dims": model_params["in_dims"], "padding": 0, "noise": 0.25, "which": 0},
     "datasets": [],
     "dataloaders": [],
     "loss_w": (0.0, 0.0, 1.0),  # Loss weights (Cross-Entropy (CE), MSE for attention, CE last label)
-    "loss_s": (None, None),  # Loss slices (CE, MSE for attention)
+    "loss_s": (0, None),  # Loss slices (CE, MSE for attention)
     "has_prompt": False,  # has prompt or not (only used for top-down Search)
 }
 
 model_params["n_tasks"] = len(tasks)
-results_folder, logger = startup_folders(r"./results", name=f"exp_a_{argus.exase}")
+results_folder, logger = startup_folders(r"./results", name=f"{argus.exase}_")
 for i, k in enumerate(tasks):
     assert tasks[k]["key"] == i, f"Key {tasks[k]['key']} must be equal to index {i}!"
 (argus.verbose == 1) and logger.info(f"train_params\n {pformat(train_params)}")
@@ -116,12 +116,12 @@ model = AttentionModel(**model_params)
 (argus.verbose == 1) and logger.info(f"Model has {get_n_parameters(model):,} parameters!")
 optimizer = torch.optim.Adam(model.parameters(), lr=train_params["lr"], weight_decay=train_params["l2"])
 scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=train_params["milestones"], gamma=train_params["gamma"])
-conductor = AttentionTrain(model, optimizer, scheduler, tasks, logger, results_folder)
+conductor = AttentionTrain(model, optimizer, scheduler, tasks, logger, results_folder, train_params["max_grad_norm"], True)
 
 # training...
 plot_all(10, model, tasks, results_folder, "_pre", DeVice, logger, (argus.verbose == 1))
 conductor.eval(DeVice)
-conductor.train(train_params["n_epochs"], DeVice, True, train_params["lm"])
+conductor.train(train_params["n_epochs"], DeVice, True)
 plot_loss_all(conductor, results_folder)
 conductor.eval(DeVice)
 plot_all(10, model, tasks, results_folder, "_post", DeVice, logger, False)
@@ -137,7 +137,7 @@ for i, task in enumerate(tasks):
     save_results_to_csv(conductor.loss_records[i], 
                         os.path.join(results_folder, f"loss_{task}.csv"),
                         ["labels", "masks", "last_label"], logger)
-    save_results_to_csv(conductor.eval_records[i], 
-                        os.path.join(results_folder, f"eval_{task}.csv"),
+    save_results_to_csv(conductor.valid_records[i], 
+                        os.path.join(results_folder, f"valid_{task}.csv"),
                         ["CEi", "CEe", "PixErr", "AttAcc", "ClsAcc"], logger)
 (argus.verbose == 1) and logger.info("Done!")
