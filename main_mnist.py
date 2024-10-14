f9df8eecc3478e67af05d994d718a21bfb664d67
Saeed Salehi <saeed.salehi.n@gmail.com>
2024-10-14 13:32:26 +0200

    Update main_mnist.py

--- main_mnist.py
+++ main_mnist.py
@@ -30,7 +30,7 @@ parser.add_argument('-l2', type=float, default=1e-6)
 parser.add_argument('-exase', type=str, default="default")
 parser.add_argument('-verbose', type=int, default=1)
 argus = parser.parse_args()
-data_path = r"../attention/data"
+data_path = r"./data"
 train_params = {
     "n_epochs": argus.n_epochs,
     "batch_size": argus.batch_size,
